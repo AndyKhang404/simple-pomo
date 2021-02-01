6fec5aed06deaa900a61134a14a956ec2a8a3b27
AndyKhang404 <andykhang404@gmail.com>
2021-02-01 16:40:30 +0700

    refactor: minor refacting

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-var defaultConfig = {
-  '1':{ s:10, m:0, name:'Work'},
-  '2':{ s:0, m:5, name:'Short break'},
-  '3':{ s:0, m:1, name:'Long break'},
-  sequence:[1,2,3],
+var config = {
+  1:{ s:10, m:0, name:'Work'},
+  2:{ s:0, m:5, name:'Short break'},
+  3:{ s:0, m:1, name:'Long break'},
+  sequence:[1,2,1,2,1,2,1,3],
 };
-
-var config = defaultConfig;
 var isRunning = false;
 var pomo_btn, pomo_timer, pomo_indicator, pomo_title, timer, sequencePos = 0;
 var s,m;
-var startAudio, pauseAudio;
+var startAudio, pauseAudio, ringAudio;
 
-window.addEventListener("load", () => {
-  startAudio = document.getElementById("startAudio");
-  pauseAudio = document.getElementById("pauseAudio");
+window.addEventListener("load", function() {
+  startAudio = document.getElementById("start-audio");
+  pauseAudio = document.getElementById("pause-audio");
+  ringAudio = document.getElementById("ring-audio");
 
   pomo_btn = document.getElementById("pomo-btn");
   pomo_timer = document.getElementById("pomo-timer");
@@ -24,9 +23,16 @@ window.addEventListener("load", () => {
   pomo_timer.innerHTML = convert(m, s);
 });
 
+window.addEventListener("keydown", function(event) {
+  if (event.code == "Space") {
+    start();
+  }
+});
+
 function start() {
   if (isRunning) {
     stop();
+    pauseAudio.play();
     isRunning = false;
   } else {
     startAudio.play();
@@ -35,7 +41,7 @@ function start() {
     pomo_title.innerHTML = convert(m, s) + ' - ' + config[config.sequence[sequencePos]].name;
     isRunning = true;
 
-    timer = setInterval(() => {
+    timer = setInterval(function() {
       s -= 1;
 
       if (s < 0) {
@@ -51,6 +57,7 @@ function start() {
         pomo_title.innerHTML = convert(m,s) + ' - ' + config[config.sequence[sequencePos]].name;
         pomo_indicator.innerHTML = config[config.sequence[sequencePos]].name;
         stop();
+        ringAudio.play();
       } else {
         pomo_timer.innerHTML = convert(m, s);
         pomo_title.innerHTML = convert(m, s) + ' - ' + config[config.sequence[sequencePos]].name;
@@ -62,19 +69,10 @@ function start() {
 
 function stop() {
   isRunning = false;
-  pauseAudio.play();
   pomo_btn.style.color = "#a11b1b";
   if (timer) clearInterval(timer);
 }
 
-function sequencePause() {
-  
-}
-
 function convert(m, s) {
-  return (
-    `0${m}`.slice(-2) +
-    ":" +
-    `0${s}`.slice(-2)
-  );
+  return ('0'+m).slice(-2) + ":" + ('0'+s).slice(-2);
 }
